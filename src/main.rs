67adf7007a1d3ab27471462e9f9fbbfc6724f920
Jessica <jesjimenez@estudiantec.cr>
2024-04-01 10:17:27 -0600

    Ventana 1

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,10 @@
-use eframe::{egui::{CentralPanel, CtxRef}, epi::App, run_native, NativeOptions};
-use std::iter::FromIterator;
+use fltk:: { app, prelude::*, window};
 
-struct Headlines {
-    articles: Vec<NewsCardData>,
-}
+fn main () { 
+    let a = app:: App:: default();
+    let mut win = window::Window::new(100,100,400,300, "Community Music Player");
+    win.end();
+    win.show();
+    a.run().unwrap();
+ }
 
-impl Headlines {
-    fn new() -> Headlines {
-        let iter = (0..20).map(|a| NewsCardData {
-            title: format!("title {}", a),
-            desc: format!("desc {}", a),
-        });
-        Headlines {
-            articles: Vec::from_iter(iter),
-        }
-    }
-}
-
-struct NewsCardData {
-    title: String,
-    desc: String,
-}
-
-impl App for Headlines {
-    fn update(&mut self, ctx: &CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
-        CentralPanel::default().show(ctx, |ui| {
-            for a in &self.articles {
-                ui.label(&a.title);
-                ui.label(&a.desc);
-            }
-        });
-    }
-
-    fn name(&self) -> &str {
-        "Headlines"
-    }
-}
-
-fn main() {
-    let app = Headlines::new();
-    let native_options = NativeOptions::default();
-    run_native(Box::new(app), native_options);
-}
