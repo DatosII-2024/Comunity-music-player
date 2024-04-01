921751568e975d9105731dba84440dd13ad59927
Jessica <jesjimenez@estudiantec.cr>
2024-04-01 00:12:02 -0600

    Pantalla

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
-fn main() {
-    println!("Hello, world");
+use eframe::{egui::{CentralPanel, CtxRef}, epi::App, run_native, NativeOptions};
+use std::iter::FromIterator;
+
+struct Headlines {
+    articles: Vec<NewsCardData>,
 }
 
-fn Prueba() {
-    println!("Hello, world");
+impl Headlines {
+    fn new() -> Headlines {
+        let iter = (0..20).map(|a| NewsCardData {
+            title: format!("title {}", a),
+            desc: format!("desc {}", a),
+        });
+        Headlines {
+            articles: Vec::from_iter(iter),
+        }
+    }
+}
+
+struct NewsCardData {
+    title: String,
+    desc: String,
+}
+
+impl App for Headlines {
+    fn update(&mut self, ctx: &CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
+        CentralPanel::default().show(ctx, |ui| {
+            for a in &self.articles {
+                ui.label(&a.title);
+                ui.label(&a.desc);
+            }
+        });
+    }
+
+    fn name(&self) -> &str {
+        "Headlines"
+    }
+}
+
+fn main() {
+    let app = Headlines::new();
+    let native_options = NativeOptions::default();
+    run_native(Box::new(app), native_options);
 }
