b3057e91d5d5483a63badff44d4cc56ab9d40de1
Gènesis <genemonte05@gmail.com>
2024-04-02 01:08:31 -0600

    arreglos de interfaz

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use fltk::{app, prelude::*, window, button}; // Importa el módulo button
+use fltk::{app, prelude::*, window, button}; // Importa los módulos necesarios
 
 fn main() {
     let app = app::App::default();
-    let mut win = window::Window::new(100, 100, 400, 300, "Community Music Player");
+    let mut win = window::Window::new(70, 60, 600, 500, "Community Music Player");
 
-    let mut btn = button::Button::new(160, 120, 80, 40, "Botón"); // Crea un nuevo botón
-    btn.set_color(fltk::enums::Color::from_u32(0xFFE4C4)); // Establece el color del botón (Opcional)
-    btn.set_label_size(20); // Establece el tamaño del texto del botón (Opcional)
+    let background_color = win.color(); // Obtener el color de fondo de la ventana
+
+    let mut btn_upvote = button::Button::new(150, 30, 80, 40, "Vote up");
+    btn_upvote.set_color(fltk::enums::Color::from_u32(0xFFE4C4));
+    btn_upvote.set_label_size(15);
+
+    let mut btn_downvote = button::Button::new(250, 30, 80, 40, "Vote down");
+    btn_downvote.set_color(fltk::enums::Color::from_u32(0xFFE4C4));
+    btn_downvote.set_label_size(15);
+
+    let mut btn_playlist = button::Button::new(350, 30, 80, 40, "Playlist");
+    btn_playlist.set_color(fltk::enums::Color::from_u32(0xFFE4C4));
+    btn_playlist.set_label_size(15);
+
+    let mut title = button::Button::new(10, 100, 100, 30, "Artista");
+    title.clear_visible_focus();
+    title.set_frame(fltk::enums::FrameType::FlatBox);
+    title.set_label_size(15);
+
+    // Establecer el color de fondo del botón al color de fondo de la ventana
+    title.set_color(background_color);
+
+    title.set_label_color(fltk::enums::Color::Black);
 
     win.end();
     win.show();
 
     app.run().unwrap();
 }
-
